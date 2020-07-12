4003c878edd10dc7b20eeb40d3b2b69598b6fa52
小宮慎ノ介 <komiyamakotonosuke@Shinnosuke-Komiya.local>
2020-07-12 23:03:33 +0900

    feat: 5章まで

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,41 @@
 class BoardsController < ApplicationController
-	def index
-	end
+    def index
+      @boards = Board.all
+    end
 
-	def new
-	end
+		def new
+			@board = Board.new
+    end
+
+    def create
+      @board = Board.new(params_board)
+      @board.save
+      redirect_to board_url(@board)
+    end
+
+    def show
+      @board = Board.find(params[:id])
+    end
+
+    def edit
+      @board = Board.find(params[:id])
+    end
+
+    def update
+      @board = Board.find(params[:id])
+      @board.update_attributes(params_board)
+      redirect_to board_url(@board)
+    end
+
+    def destroy
+      @board = Board.find(params[:id])
+      @board.destroy
+      redirect_to boards_url
+    end
+
+    private
+
+    def params_board
+      params.require(:board).permit(:title, :editor)
+    end
 end
